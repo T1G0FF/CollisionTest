0114b8f6953fec9140ef63cae2e8166283ec1111
Thomas <thomas.gitcommit@outlook.com>
2015-07-31 23:04:54 +1000

    Added Health and Damage mechanics

--- CollisionTest/CollisionTest.cs
+++ CollisionTest/CollisionTest.cs
@@ -14,6 +14,8 @@ namespace CollisionTest
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        SpriteFont Consolas14;
+        SpriteFont Consolas56;
         
         private InputState _inputState;
         private Rectangle mapBounds;
@@ -63,6 +65,18 @@ namespace CollisionTest
                 new Obstacle(this, new Rectangle(rand.Next(mapBounds.Width),-rand.Next(mapBounds.Height),rand.Next(25,100),rand.Next(25,100)), 0, rand.Next(5,15))
             };
 
+            foreach (Obstacle block in blocks)
+            {
+                block.Damage.Generic = 10;
+            }
+
+            foreach(Player p in players)
+            {
+                p.Status.Invunerable = false;
+                p.Health.Max = 100;
+                p.Health.Current = p.Health.Max;
+            }
+
             base.Initialize();
         }
 
@@ -73,6 +87,8 @@ namespace CollisionTest
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
+            Consolas14 = Content.Load<SpriteFont>("Consolas14");
+            Consolas56 = Content.Load<SpriteFont>("Consolas56");
 
             foreach (Player player in players)
             {
@@ -101,11 +117,6 @@ namespace CollisionTest
             if (_inputState.IsCancel(PlayerIndex.One))
             {
                 Exit();
-            }            
-            
-            foreach (Player p in players)
-            {
-                p.HandleInput(_inputState, p._index, mapBounds, blocks);
             }
 
             foreach (Obstacle b in blocks)
@@ -120,6 +131,21 @@ namespace CollisionTest
 
             foreach (Player p in players)
             {
+                p.UpdateMovement(_inputState, p._index);
+                bool collideMap = p.CheckBounds(mapBounds);
+                int collidedBlocks = p.CheckCollisions(blocks);
+
+                if (collideMap && collidedBlocks != 0 && !p.Status.Invunerable)
+                {
+                    p.Health.Current = p.Health.Current - blocks[0].Damage.Generic;
+                }
+
+                if (p.CurrentState == Player.State.Dead && _inputState.IsPause(p._index))
+                {
+                    p.Health.Current = p.Health.Max;
+                    p.CurrentState = Player.State.Walking;
+                }
+
                 p.Update(gameTime);
             }
 
@@ -136,16 +162,35 @@ namespace CollisionTest
 
             spriteBatch.Begin();
 
-            foreach (Player player in players)
-            {
-                player.Draw(spriteBatch, player._color);
-            }
-
             foreach (Obstacle block in blocks)
             {
                 block.Draw(spriteBatch, Color.Gray, 1.0F);
             }
 
+            string healthString = "";
+            foreach (Player player in players)
+            {
+                if (player.CurrentState == Player.State.Dead)
+                {
+                    string gameOverString = "GAME OVER!";
+                    // Fits Text in centre of screen
+                    Vector2 textSize = Consolas56.MeasureString(gameOverString)/2;
+                    Vector2 centerScreen = new Vector2(graphics.GraphicsDevice.Viewport.Width / 2, graphics.GraphicsDevice.Viewport.Height / 2);
+                    Vector2 textPosition = centerScreen - textSize;
+                    spriteBatch.DrawString(Consolas56, gameOverString, textPosition, Color.Red, 0.0F, Vector2.Zero, 1.0F, SpriteEffects.None, 1.0F);
+                }
+                else
+                {
+                    player.Draw(spriteBatch, player._color);
+                    healthString += string.Format("Player {0}: {1:P}", player._index, player.Health.Percent);
+                    healthString += System.Environment.NewLine;
+                }
+            }
+            // Fits Text just above bottom of screen and removes final line-break
+            float textWidth = Consolas14.MeasureString(healthString).Y;
+            textWidth = textWidth - (textWidth / (players.Length + 1));
+            spriteBatch.DrawString(Consolas14, healthString, new Vector2(0, graphics.GraphicsDevice.Viewport.Height - textWidth), Color.White);
+            
             spriteBatch.End();
 
             base.Draw(gameTime);
